3cd19e973f79f63d675a2491054d2adb4496ea3e
Salvatore Castello <93254988+SalvatoreCastello@users.noreply.github.com>
2022-10-10 19:51:08 +0200

    Update extract.ml

--- ocaml-basics/extract/extract.ml
+++ ocaml-basics/extract/extract.ml
@@ -8,7 +8,7 @@ let rec extractor i = function
    arriva a 0, restituisce la lista originale a meno dell'elemento in testa al
    momento del counter=0 *)
 let rec pop i = function
-    [] -> []
+    [] -> failwith "Index out of bounds."
   | h::l -> if i=0 then l else h::pop (i-1) l;;
 
 (* Funzione richiesta, restituisce la coppia di elemento estratto e lista *)
